66db8d9f15fcf7a9eba9ba18cab42dce0a2488d8
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-04-18 15:53:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,32 +9,37 @@ const {
   userLeave,
   getRoomUsers
 } = require('./utils/users');
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const { port, imageBot } = require('./utils/config');
+const config = require('./utils/config');
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'FARMMP Bot';
+const botName = 'FARMP Bot';
 
 // Run when client connects
 io.on('connection', socket => {
   socket.on('joinRoom', ({ username, room, image }) => {
+    if(image === undefined || image === '' || image.onerror) {
+      image = config.config.image
+    }
     const user = userJoin(socket.id, username, room, image);
 
+    console.log(user)
+
     socket.join(user.room);
+
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to FARMMP!', imageBot));
+    socket.emit('message', formatMessage(botName, `Hello ${user.username}, Welcome in FARMP Chat`, config.config.imageBot));
 
     // Broadcast when a user connects
     socket.broadcast
       .to(user.room)
       .emit(
         'message',
-        formatMessage(botName, `${user.username} has joined the chat`, imageBot)
+        formatMessage(botName, `${user.username} has joined the chat`, config.config.image)
       );
 
     // Send users and room info
@@ -42,11 +47,17 @@ io.on('connection', socket => {
       room: user.room,
       users: getRoomUsers(user.room)
     });
+    
   });
 
   // Listen for chatMessage
-  socket.on('chatMessage', (msg) => {
-    const user = getCurrentUser(socket.id);
+  socket.on('chatMessage', msg => {
+    var user = getCurrentUser(socket.id);
+    if(user === undefined) return socket.emit('message', 
+    formatMessage(botName, `User is not defined restart your session!`, config.config.imageBot));
+   if(msg.includes('https') || msg.includes('http')) return socket.emit('message', 
+   formatMessage(botName, `You don't have the permission to content a link!`, config.config.imageBot));
+    console.log(formatMessage(user.username, msg, user.image))
     io.to(user.room).emit('message', formatMessage(user.username, msg, user.image));
   });
 
@@ -57,7 +68,7 @@ io.on('connection', socket => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        formatMessage(botName, `${user.username} has left the chat`, imageBot)
+        formatMessage(botName, `${user.username} has left the chat`, config.config.image)
       );
 
       // Send users and room info
@@ -67,8 +78,10 @@ io.on('connection', socket => {
       });
     }
   });
+
 });
 
-const PORT = process.env.PORT || port;
+
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
